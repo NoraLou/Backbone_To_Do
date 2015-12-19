ede8895542fe2e820c551c4bf42787e94d26f5c8
NoraLou <nora.humpage@gmail.com>
2015-12-19 14:09:43 -0800

    can add..

--- js/TodoItemView.js
+++ js/TodoItemView.js
@@ -1,5 +1,8 @@
 
 var TodoItemView = Backbone.View.extend({
+
+  tagName:"li",
+
   initialize: function(options){
     if(!(options && options.model)){
       throw new Error("model is not specified!");
--- js/TodoItemsView.js
+++ js/TodoItemsView.js
@@ -1,14 +1,38 @@
 var TodoItemsView = Backbone.View.extend({
 
+  tagName: "ul",
+
+  id: "todoItems",
+
   initialize: function(options){
     if (!(options && options.collection))
       throw new Error("collection is not specified.");
+
+    this.collection.on("add", this.onAddTodo, this)
+  },
+
+  onAddTodo: function(todoItem){
+    var view = new TodoItemView({model: todoItem});
+    this.$el.append(view.render().$el);
+  },
+
+  events:{
+    "click #add" : "addTodo"
+  },
+
+  addTodo: function(){
+    var $newTask = $("#newTodoItem");
+    var todoItem = new TodoItem({description: $newTask.val()});
+    this.collection.add(todoItem);
+    $newTask.val('');
   },
 
   render: function(){
+    this.$el.append("<input type='text' id='newTodoItem'></input>")
+
+    this.$el.append("<button id='add'>ADD</button>");
+
     var self = this;
-    console.log("this.model :", this.model)
-    console.log("this.collection :", this.collection)
     this.collection.each(function(todoItem){
       var view = new TodoItemView({ model: todoItem });
       self.$el.append(view.render().$el);
